1a0edd606a05435bf6156d9b182709b958bb7b59
Ralph Hogaboom <ralph@hogaboom.org>
2024-06-01 15:37:42 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,33 @@
+# setup.py
+
+# check for database
+import os
+import sqlite3
+from sqlite3 import Error
+from listing import Listing
+
+db = "db/db.sqlite3"
+if os.path.exists(db):
+    print("Database already exists, moving on ...")
+else:
+    # create database
+    connection = sqlite3.connect(db)
+    cursor = connection.cursor()
+    connection = sqlite3.connect(db)
+    cursor = connection.cursor()
+
+    if os.path.exists("db/newdb.sql"):
+        with open("db/newdb.sql", "r") as sql_file:
+            sql = sql_file.read()
+        cursor.execute(sql)
+        connection.commit()
+        connection.close()
+    else:
+        print("SQL script to initiate the database not found. Are you sure you cloned the git repo?")
+        exit()
+
+# index scanners
+# scanners = os.listdir("scanners/")
+# for scanner in scanners:
+    # get list of scanner files
+    # get list of scanners in db
