be2a97f9eaaf6fa3e3a9dda65a0ef9d85d7e2a5a
Ralph Hogaboom <ralph@hogaboom.org>
2024-06-05 23:18:24 -0700

    Delete main.bat

--- main.bat
+++
@@ -1,5 +0,0 @@
-python scanners/scanner_burkhamer.py
-python scanners/scanner_cathysrentals.py
-python scanners/scanner_facebookmarketplace.py
-python scanners/scanner_ssrealty.py
-python scanners/scanner_spivey.py
